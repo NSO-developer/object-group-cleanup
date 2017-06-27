cfda2dcb7776fb87bc1fe12b3f2b9c035b602341
divyarao <divyarao@cisco.com>
2017-06-27 13:53:07 -0700

    checked if og existed in device's acl

--- obj_cleanup.py
+++ obj_cleanup.py
@@ -3,6 +3,28 @@
 import ncs
 import socket
 
+def find_rule_in_ACL (box, og, root, ACL):
+    """
+    This funcion will iterate through the rules in the ACL to find matches of the given OG. Returns boolean
+    values
+    """
+    for rule in root.devices.device[box].config.access_list[ACL]:
+        if og in rule:
+            return true         #return true for continuous iteration
+    return false                #return false for flagging for removal
+
+def find_in_ACLS(box, og, root):
+    """
+    Function will iterate through the ACL's within device and return true if found (keep)
+    and false if not found (remove)
+    """
+    flag = false
+    for acl in root.devices.device[box].config.access_list.extended:
+        flag = find_rule_in_ACL(box, og, root, acl)
+        if flag == true:
+            break
+    return flag            #return true if found, false if not
+
 def flag_ogs_in_box(box):
     """
     This function returns a list of object groups that are orphaned in that device.
@@ -14,28 +36,3 @@ def flag_ogs_in_box(box):
             if (!find_in_ACLS(box,og,root)):
                 orphaned_og.append(str(og))
     return orphaned_og
-
-def find_in_ACLS(box, og, root):
-    """
-    Function will iterate through the ACL's within device and return true if found (keep)
-    and false if not found (remove)
-    """
-    flag = false
-
-    for acl in root.devices.device[box].config.access_list.extended:
-        flag = find_rule_in_ACL(box, og, root, acl)
-
-        if flag == true:
-            break
-
-        return flag            #return true if found, false if not
-
-def find_rule_in_ACL (box, og, root, ACL):
-    """
-    This funcion will iterate through the rules in the ACL to find matches of the given OG. Returns boolean
-    values
-    """
-    for rule in root.devices.device[box].config.access_list[ACL]:
-        if og in rule:
-            return true         #return true for continuous iteration
-    return false                #return false for flagging for removal
