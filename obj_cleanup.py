5b45b265fb5ca423324ca86be17e62330dd2b22e
divyarao <divyarao@cisco.com>
2017-06-27 17:14:34 -0700

    debugged

--- obj_cleanup.py
+++ obj_cleanup.py
@@ -8,20 +8,20 @@ def find_rule_in_ACL (box, og, root, ACL):
     This funcion will iterate through the rules in the ACL to find matches of the given OG. Returns boolean
     values
     """
-    for rule in root.devices.device[box].config.access_list[ACL]:
-        if og in rule:
-            return true         #return true for continuous iteration
-    return false                #return false for flagging for removal
+    for rul in root.devices.device[box].config.asa__access_list.access_list_id[ACL.id].rule:
+        if og in rul:
+            return True         #return true for continuous iteration
+    return False                #return false for flagging for removal
 
 def find_in_ACLS(box, og, root):
     """
     Function will iterate through the ACL's within device and return true if found (keep)
     and false if not found (remove)
     """
-    flag = false
-    for acl in root.devices.device[box].config.access_list.extended:
+    flag = False
+    for acl in root.devices.device[box].config.asa__access_list.access_list_id:
         flag = find_rule_in_ACL(box, og, root, acl)
-        if flag == true:
+        if flag:
             break
     return flag            #return true if found, false if not
 
@@ -30,9 +30,16 @@ def flag_ogs_in_box(box):
     This function returns a list of object groups that are orphaned in that device.
     """
     orphaned_og = []
-    with ncs.maapi.single_write_trans('admin','python') as t:
-        root = ncs.maapi.get_root(t)
-        for og in root.devices.device[box].config.object-group:
-            if (!find_in_ACLS(box,og,root)):
-                orphaned_og.append(str(og))
+    with ncs.maapi.single_write_trans('ncsadmin', 'python', groups=['ncsadmin']) as t:
+        root = ncs.maagic.get_root(t)
+        #print(dir(root.devices.device[box].config.asa__object_group))
+        for typ in root.devices.device[box].config.asa__object_group:
+            print root.devices.device[box].config.asa__object_group[typ]
+            for og in root.devices.device[box].config.asa__object_group[typ]:
+                print og
+                if not find_in_ACLS(box,og,root):
+                    orphaned_og.append(str(og))
     return orphaned_og
+
+if __name__ == "__main__":
+    print flag_ogs_in_box('svl-gem-joe-asa-fw1.cisco.com')
