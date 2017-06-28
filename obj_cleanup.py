803fc876e50d4f64b89807fbae0fe0ac31ea39ac
divyarao <divyarao@cisco.com>
2017-06-28 11:16:39 -0700

    checking for ogs in acls

--- obj_cleanup.py
+++ obj_cleanup.py
@@ -2,14 +2,15 @@
 
 import ncs
 import socket
-
+#/ ncs:devices / device { svl-gem-joe-asa-fw1.cisco.com } / config / asa:access-list / access-list-id { gem_ft_apt }
 def find_rule_in_ACL (box, og, root, ACL):
     """
     This funcion will iterate through the rules in the ACL to find matches of the given OG. Returns boolean
     values
     """
     for rul in root.devices.device[box].config.asa__access_list.access_list_id[ACL.id].rule:
-        if og in rul:
+        #print rul.id
+        if og in root.devices.device[box].config.asa__access_list.access_list_id[ACL.id].rule[rul.id]:
             return True         #return true for continuous iteration
     return False                #return false for flagging for removal
 
@@ -19,7 +20,9 @@ def find_in_ACLS(box, og, root):
     and false if not found (remove)
     """
     flag = False
+    #/ ncs:devices / device { svl-gem-joe-asa-fw1.cisco.com } / config / asa:access-list / access-list-id { jw_apt }
     for acl in root.devices.device[box].config.asa__access_list.access_list_id:
+        print acl.id
         flag = find_rule_in_ACL(box, og, root, acl)
         if flag:
             break
@@ -29,16 +32,16 @@ def flag_ogs_in_box(box):
     """
     This function returns a list of object groups that are orphaned in that device.
     """
+    #og_type = ['icmp_type','network','service','user']
     orphaned_og = []
     with ncs.maapi.single_write_trans('ncsadmin', 'python', groups=['ncsadmin']) as t:
         root = ncs.maagic.get_root(t)
-        #print(dir(root.devices.device[box].config.asa__object_group))
-        for typ in root.devices.device[box].config.asa__object_group:
-            print root.devices.device[box].config.asa__object_group[typ]
-            for og in root.devices.device[box].config.asa__object_group[typ]:
-                print og
-                if not find_in_ACLS(box,og,root):
-                    orphaned_og.append(str(og))
+        grp = root.devices.device[box].config.asa__object_group
+        for ogtyp in grp:
+            for og in root.devices.device[box].config.asa__object_group['asa:service']:
+                if not find_in_ACLS(box,og.id,root):
+                    #print og.id
+                    orphaned_og.append(og.id)
     return orphaned_og
 
 if __name__ == "__main__":
