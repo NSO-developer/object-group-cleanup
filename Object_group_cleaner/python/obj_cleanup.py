be3047c9038e5f59c199095a965cd5169d55d7f9
axperez <axperez@cisco.com>
2017-07-26 16:44:21 -0700

    error message update: stat

--- Object_group_cleaner/python/obj_cleanup.py
+++ Object_group_cleaner/python/obj_cleanup.py
@@ -146,24 +146,18 @@ def banish(og, acl):
             return True
     return False
 
-def remove_ogs(obj_groups):
+def remove_ogs(box, og_id, og_type):
     """
     A function that removes the object group from the object group list using
     the arguments passed: device name, object group name, and object group type.
     """
     with ncs.maapi.single_write_trans('ncsadmin', 'python', groups=['ncsadmin']) as t:
-        root = ncs.maagic.get_root(t)
-
-        for obj in obj_groups:
-
         del root.devices.device[box].config.asa__object_group[og_type][og_id]
         try:
             t.apply()
             stat = "Success"
         except:
             stat = "Error Removing"
-
-    return stat
 
 
 def no_ogs_error(box):
