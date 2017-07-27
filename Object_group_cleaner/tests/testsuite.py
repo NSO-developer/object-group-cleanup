875f8e0c12ad8f913eab87c75dfca53c6ebc7101
divyarao <divyarao@cisco.com>
2017-07-27 11:26:39 -0700

    defined tests

--- Object_group_cleaner/tests/testsuite.py
+++ Object_group_cleaner/tests/testsuite.py
@@ -1,4 +1,20 @@
 import unittest
 sys.path.append('../python')
 import obj_cleanup
-class TestOGC(unittest):
+import ncs
+
+class TestOGC(unittest.TestCase):
+
+    def test_search_empty(self):
+        with ncs.maapi.single_write_trans('ncsadmin', 'python', groups=['ncsadmin']) as t:
+            root = ncs.maagic.get_root(t)
+            root.Object_group_cleaner.search.input.("device","asa-netsim-1")
+
+    def test_seach_reg(self):
+
+    def test_perf(self):
+
+    def test_remove(self):
+
+if __name__ == '__main__':
+    unittest.main()
