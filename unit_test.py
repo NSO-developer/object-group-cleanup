5ce0708d1edce0b438a960c70d19879c3568cc88
divyarao <divyarao@cisco.com>
2017-07-31 16:15:36 -0700

    testing output

--- unit_test.py
+++ unit_test.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
 
     device_typ = "device"
     device_name = "asa-netsim-1"
+    ret = {}
     with ncs.maapi.single_write_trans('ncsadmin', 'python', groups=['ncsadmin']) as t:
         root = ncs.maagic.get_root(t)
         device = root.devices.device[device_name]
@@ -22,10 +23,21 @@ if __name__ == '__main__':
 
         end_time = output1.end_time
         org_gps = output1.orphaned_object_groups
+
+        for og in org_gps:
+            if og.og_type in ret.keys():
+                ret[og.og_type].append(og.object_group)
+            #Else, create key and append og
+            else:
+                ret[og.og_type] = [og.object_group]
+
+        print ret
+
         run_time = output1.run_time
         start_time = output1.start_time
-
+        """
         print run_time
         print output1.orphaned_object_groups
         print dir(output1.orphaned_object_groups)
         print type(output1.orphaned_object_groups)
+"""
