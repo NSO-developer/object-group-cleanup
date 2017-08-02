1c45e201fe28a692ae75b9cff9a48733449cdf2b
axperez <axperez@cisco.com>
2017-08-02 09:28:49 -0700

    test suites dev

--- Object_group_cleaner/tests/testsuite.py
+++ Object_group_cleaner/tests/testsuite.py
@@ -1,16 +1,95 @@
 import unittest
-sys.path.append('../python')
-import obj_cleanup
+import constants
+#sys.path.append('../python')
+#import obj_cleanup
 import ncs
 import socket
-import constants
+import time
+
 
 class TestOGC(unittest.TestCase):
 
     def test_search_empty(self):
-        """
-        This function tests if the device is returning the correct output from the Object_group_cleaner tool.
-        """
+
+        orphaned_ogs = {}
+        empty_dict = {}
+
+        with ncs.maapi.Maapi() as m:
+            with ncs.maapi.Session(m, 'ncsadmin', 'python', groups=['ncsadmin']):
+
+                with m.start_write_trans() as t:
+                    root = ncs.maagic.get_root(t)
+
+                    og = "test_og_"
+
+                    num_types = 0
+                    for ogtyp in root.devices.device[constants.device_name].config.asa__object_group:
+                        og_num = og + str(num_types) + '_'
+                        num_types = num_types + 1
+                        for j in range(20):
+                            fake_og = og_num + str(j)
+                            root.devices.device[constants.device_name].config.asa__object_group[ogtyp].create(fake_og)
+
+                    t.apply()
+
+                with m.start_write_trans() as t:
+                    root = ncs.maagic.get_root(t)
+
+                    holder = "access_list_"
+                    rul = "extended permit icmp object-group test_og_"
+
+                    for i in range(num_types):
+                        acl_num = holder + str(i)
+                        root.devices.device[constants.device_name].config.asa__access_list.access_list_id.create(acl_num)
+                        rul_num = rul + str(i) + '_'
+                        for j in range(20):
+                            fake_rule = rul_num + str(j)
+                            root.devices.device[constants.device_name].config.asa__access_list.access_list_id[acl_num].rule.create(fake_rule)
+
+                    t.apply()
+
+
+                with m.start_write_trans() as t:
+
+                    root = ncs.maagic.get_root(t)
+                    device = root.devices.device[constants.device_name]
+                    input1 = root.Object_group_cleaner.search.get_input()
+                    new_obj = input1.inputs.create()
+                    new_obj.input_type = constants.device_typ
+                    new_obj.value = constants.device_name
+
+                    output1 = root.Object_group_cleaner.search(input1)
+
+                    end_time = output1.end_time
+                    org_gps = output1.orphaned_object_groups
+
+                    for og in org_gps:
+                        if og.og_type in orphaned_ogs.keys():
+                            orphaned_ogs[og.og_type].append(og.object_group)
+                        else:
+                            orphaned_ogs[og.og_type] = [og.object_group]
+
+                    self.assertEqual(orphaned_ogs, empty_dict)
+
+                with m.start_write_trans() as t:
+                    root = ncs.maagic.get_root(t)
+                    for ogtyp in root.devices.device["asa-netsim-1"].config.asa__object_group:
+                        for og in root.devices.device["asa-netsim-1"].config.asa__object_group[ogtyp]:
+                            del root.devices.device["asa-netsim-1"].config.asa__object_group[ogtyp][og.id]
+
+                    t.apply()
+
+                with m.start_write_trans() as t:
+                    root = ncs.maagic.get_root(t)
+                    for acl in root.devices.device["asa-netsim-1"].config.asa__access_list.access_list_id:
+                        del root.devices.device["asa-netsim-1"].config.asa__access_list.access_list_id[acl.id]
+
+                    t.apply()
+
+
+
+
+    def test_seach_reg(self):
 
         orphaned_ogs = {}
 
@@ -20,10 +99,25 @@ class TestOGC(unittest.TestCase):
                 with m.start_write_trans() as t:
                     root = ncs.maagic.get_root(t)
 
+                    og = "test_og_"
+
+                    num_types = 0
+                    for ogtyp in root.devices.device[constants.device_name].config.asa__object_group:
+                        og_num = og + str(num_types) + '_'
+                        num_types = num_types + 1
+                        for j in range(50):
+                            fake_og = og_num + str(j)
+                            root.devices.device[constants.device_name].config.asa__object_group[ogtyp].create(fake_og)
+
+                    t.apply()
+
+                with m.start_write_trans() as t:
+                    root = ncs.maagic.get_root(t)
+
                     holder = "access_list_"
                     rul = "extended permit icmp object-group test_og_"
 
-                    for i in range(4):
+                    for i in range(num_types):
                         acl_num = holder + str(i)
                         root.devices.device[constants.device_name].config.asa__access_list.access_list_id.create(acl_num)
                         rul_num = rul + str(i) + '_'
@@ -34,28 +128,13 @@ class TestOGC(unittest.TestCase):
                     t.apply()
 
                 with m.start_write_trans() as t:
-                    root = ncs.maagic.get_root(t)
-
-                    og = "test_og_"
-
-                    i = 0
-                    for ogtyp in root.devices.device[constants.device_name].config.asa__object_group:
-                        og_num = og + str(i) + '_'
-                        i = i + 1
-                        for j in range(50):
-                            fake_og = og_num + str(j)
-                            root.devices.device[constants.device_name].config.asa__object_group[ogtyp].create(fake_og)
-
-                    t.apply()
-
-                with m.start_write_trans() as t:
 
                     root = ncs.maagic.get_root(t)
                     device = root.devices.device[constants.device_name]
                     input1 = root.Object_group_cleaner.search.get_input()
                     new_obj = input1.inputs.create()
                     new_obj.input_type = constants.device_typ
-                    new_obj.value = device_name
+                    new_obj.value = constants.device_name
 
                     output1 = root.Object_group_cleaner.search(input1)
 
@@ -85,14 +164,80 @@ class TestOGC(unittest.TestCase):
 
                     t.apply()
 
+    def test_perform(self):
 
+        orphaned_ogs = {}
 
+        with ncs.maapi.Maapi() as m:
+            with ncs.maapi.Session(m, 'ncsadmin', 'python', groups=['ncsadmin']):
 
-    def test_seach_reg(self):
+                with m.start_write_trans() as t:
+                    root = ncs.maagic.get_root(t)
 
-    def test_perf(self):
+                    og = "test_og_"
 
-    def test_remove(self):
+                    num_types = 0
+                    for ogtyp in root.devices.device[constants.device_name].config.asa__object_group:
+                        og_num = og + str(num_types) + '_'
+                        num_types = num_types + 1
+                        for j in range(5000):
+                            fake_og = og_num + str(j)
+                            root.devices.device[constants.device_name].config.asa__object_group[ogtyp].create(fake_og)
+
+                    t.apply()
+
+                with m.start_write_trans() as t:
+                    root = ncs.maagic.get_root(t)
+
+                    holder = "access_list_"
+                    rul = "extended permit icmp object-group test_og_"
+
+                    for i in range(num_types):
+                        acl_num = holder + str(i)
+                        root.devices.device[constants.device_name].config.asa__access_list.access_list_id.create(acl_num)
+                        rul_num = rul + str(i) + '_'
+                        for j in range(4500):
+                            fake_rule = rul_num + str(j)
+                            root.devices.device[constants.device_name].config.asa__access_list.access_list_id[acl_num].rule.create(fake_rule)
+
+                    t.apply()
+
+                with m.start_write_trans() as t:
+
+                    root = ncs.maagic.get_root(t)
+                    device = root.devices.device[constants.device_name]
+                    input1 = root.Object_group_cleaner.search.get_input()
+                    new_obj = input1.inputs.create()
+                    new_obj.input_type = constants.device_typ
+                    new_obj.value = constants.device_name
+
+                    b = time.time()
+                    output1 = root.Object_group_cleaner.search(input1)
+                    af = time.time()
+                    run_time = af - b
+
+                    #run_time = output1.run_time
+                    #org_gps = output1.orphaned_object_groups
+                    print run_time
+
+                    self.assertTrue(run_time < 500)
+
+                with m.start_write_trans() as t:
+                    root = ncs.maagic.get_root(t)
+                    for ogtyp in root.devices.device["asa-netsim-1"].config.asa__object_group:
+                        for og in root.devices.device["asa-netsim-1"].config.asa__object_group[ogtyp]:
+                            del root.devices.device["asa-netsim-1"].config.asa__object_group[ogtyp][og.id]
+
+                    t.apply()
+
+                with m.start_write_trans() as t:
+                    root = ncs.maagic.get_root(t)
+                    for acl in root.devices.device["asa-netsim-1"].config.asa__access_list.access_list_id:
+                        del root.devices.device["asa-netsim-1"].config.asa__access_list.access_list_id[acl.id]
+
+                    t.apply()
+
+    #def test_remove(self):
 
 if __name__ == '__main__':
     unittest.main()
